fb4a511786cef6711dd51489fa08041933e8b957
sindhuk55 <sindhusea555@gmail.com>
2023-10-20 19:14:18 +0530

    new project

---
+++ GitSample.java
@@ -0,0 +1,15 @@
+
+public class GitSample
+{
+	public void sample() 
+	{
+		System.out.println("hello ");
+	}
+	
+	public static void main(String args[])
+	{
+		GitSample g=new GitSample();
+		g.sample();
+	}
+
+}
